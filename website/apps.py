cf7203605db239336f02d5119d6e25d51a8fe8a3
rúben <44413930+Guhzy@users.noreply.github.com>
2021-05-16 16:44:03 +0100

    Update apps.py

--- website/apps.py
+++ website/apps.py
@@ -1,14 +1,6 @@
 from django.apps import AppConfig
 
 
-class AutomatoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'website'
-
-class MaquinaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'website'
-
-class ExpressaoConfig(AppConfig):
+class WebsiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'website'
